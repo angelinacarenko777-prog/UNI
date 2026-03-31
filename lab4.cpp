adc39193e91543b025e4138bc18572b310eba4e1
Angelina Tsarenko <angelinacarenko777@gmail.com>
2026-03-31 11:50:05 +0300

    Refactor classes and add Address and Person

--- lab4.cpp
+++ lab4.cpp
@@ -1,162 +1,59 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
-class Player {
+
+class Address {
+private:
+    string city;
+    string street;
+    int houseNumber;
+
+public:
+    Address(string city, string street, int houseNumber) {
+        this->city = city;
+        this->street = street;
+        this->houseNumber = houseNumber;
+    }
+
+    void printAddress() const {
+        cout << "Місто: " << city
+             << ", Вулиця: " << street
+             << ", Будинок: " << houseNumber << endl;
+    }
+};
+
+
+class Person {
 private:
     string name;
-    int id;
+    int age;
+    Address address; 
 
 public:
-    Player(string name, int id) {
+    Person(string name, int age, Address address)
+        : address(address) {
         this->name = name;
-        this->id = id;
+        this->age = age;
     }
 
-    void printInfo() const {
-        cout << "Player{name: " << name << ", id: " << id << "}" << endl;
-    }
-};
-
-class Team {
-private:
-    string teamName;
-    vector<Player*> players;
-
-public:
-    Team(string teamName) {
-        this->teamName = teamName;
-    }
-
-    void addPlayer(Player* player) {
-        players.push_back(player);
-    }
-
-    void removePlayer(Player* player) {
-        players.erase(remove(players.begin(), players.end(), player), players.end());
-    }
-
-    void showTeamInfo() const {
-        cout << "Team: " << teamName << endl;
-
-        for (const auto& player : players) {
-            player->printInfo();
-        }
+    void printPerson() const {
+        cout << "Ім'я: " << name << endl;
+        cout << "Вік: " << age << endl;
+        address.printAddress(); 
     }
 };
 
-class Door {
-private:
-    double width;
-    double height;
-
-public:
-    Door(double width, double height) {
-        this->width = width;
-        this->height = height;
-    }
-
-    void printInfo() const {
-        cout << "Door width: " << width << " height: " << height << endl;
-    }
-};
-
-class Window {
-private:
-    double width;
-    double height;
-
-public:
-    Window(double width, double height) {
-        this->width = width;
-        this->height = height;
-    }
-
-    void printInfo() const {
-        cout << "Window width: " << width << " height: " << height << endl;
-    }
-};
-
-class Room {
-private:
-    string type;
-
-public:
-    Room(string type) {
-        this->type = type;
-    }
-
-    void printInfo() const {
-        cout << "Room type: " << type << endl;
-    }
-};
-
-class House {
-private:
-    vector<Room> rooms;
-    vector<Door> doors;
-    vector<Window> windows;
-
-public:
-    House() {
-        rooms.emplace_back("Bedroom");
-        rooms.emplace_back("Kitchen");
-        rooms.emplace_back("Bathroom");
-
-        doors.emplace_back(90, 200);
-        doors.emplace_back(80, 200);
-
-        windows.emplace_back(120, 100);
-        windows.emplace_back(150, 120);
-    }
-
-    void showHouseInfo() const {
-
-        cout << "\nHouse information:\n";
-
-        cout << "\nRooms:\n";
-        for (const auto& room : rooms) {
-            room.printInfo();
-        }
-
-        cout << "\nDoors:\n";
-        for (const auto& door : doors) {
-            door.printInfo();
-        }
-
-        cout << "\nWindows:\n";
-        for (const auto& window : windows) {
-            window.printInfo();
-        }
-    }
-};
 
 int main() {
 
-    Player p1("Alice", 1);
-    Player p2("Bob", 2);
-    Player p3("Charlie", 3);
+    Address addr("Івано-Франківськ", "Шевченка", 10);
 
-    Team team("SuperStars");
+    Person p("Ангеліна", 18, addr);
 
-    team.addPlayer(&p1);
-    team.addPlayer(&p2);
-    team.addPlayer(&p3);
-
-    cout << "Team information:\n";
-    team.showTeamInfo();
-
-    cout << "\nPlayer exists outside team:\n";
-    p1.printInfo();
-
-    House house;
-
-    house.showHouseInfo();
-
-    cout << "\nDeleting house...\n";
+    cout << "Інформація про людину:\n";
+    p.printPerson();
 
     return 0;
 }
